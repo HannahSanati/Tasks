a82f2f89b7b917343aef7bbfad8e55661b45eb87
“HannahSanati” <hannahsanatiii@gmail.com>
2025-09-16 13:40:40 +0330

    add description card

--- src/app/components/zanjan-event/zanjan-event.ts
+++ src/app/components/zanjan-event/zanjan-event.ts
@@ -3,10 +3,11 @@ import { Navbar } from "./navbar/navbar";
 import { ScheduleCard } from "./schedule-card/schedule-card";
 import { Followers } from "./followers/followers";
 import { BannerSlider } from "./banner-slider/banner-slider";
+import { Description } from "./description/description";
 
 @Component({
   selector: 'app-zanjan-event',
-  imports: [Navbar, ScheduleCard, Followers, BannerSlider],
+  imports: [Navbar, ScheduleCard, Followers, BannerSlider, Description],
   templateUrl: './zanjan-event.html',
   styleUrl: './zanjan-event.scss'
 })
