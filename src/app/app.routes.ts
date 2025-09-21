b38a71b88e593a8d261fd0f44e53c1e2adee9ddf
“HannahSanati” <hannahsanatiii@gmail.com>
2025-09-21 11:37:28 +0330

    Add routes for all my tasks

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,16 +1,27 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-//   {
-//     path: 'resource-management',
-//     loadComponent: () =>
-//       import('')
-//         .then(c => c.HomeComponent),
-//   },
-//   {
-//     path: 'zanjan-event',
-//     loadComponent: () =>
-//       import('')
-//         .then(c => c.AdminDashboardComponent),
-//   },
+  {
+    path: 'resource-management',
+    loadComponent: () =>
+      import('./components/resource-management/resource-management')
+        .then(c => c.ResourceManagement),
+  },
+  {
+    path: 'zanjan-event',
+    loadComponent: () =>
+      import('./components/zanjan-event/zanjan-event')
+        .then(c => c.ZanjanEvent),
+  },
+  {
+    path: 'authentication-flow',
+    loadComponent: () =>
+      import('./components/authentication-flow/authentication-flow')
+        .then(c => c.AuthenticationFlow),
+  },
+  {
+    path: '',
+    redirectTo: 'authentication-flow',
+    pathMatch: 'full'
+  }
 ];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,15 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Header } from "../app/components/resource-management/header/header";
-import { Table } from "../app/components/resource-management/table/table";
-import { Navbar } from "./components/zanjan-event/navbar/navbar";
-import { ScheduleCard } from "./components/zanjan-event/schedule-card/schedule-card";
-import { ZanjanEvent } from './components/zanjan-event/zanjan-event';
-import { AuthenticationFlow } from "./components/authentication-flow/authentication-flow";
+import { Header } from './components/resource-management/header/header';
+import { Table } from './components/resource-management/table/table';
+import { Navbar } from './components/zanjan-event/navbar/navbar';
+import { ScheduleCard } from './components/zanjan-event/schedule-card/schedule-card';
 
 @Component({
   selector: 'app-root',
-  imports: [ZanjanEvent, AuthenticationFlow, RouterOutlet],
+  imports: [RouterOutlet, Header, Table, Navbar, ScheduleCard,],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
@@ -18,4 +16,3 @@ export class App {
 }
 
 
-// Header, Table, Navbar, ScheduleCard,
